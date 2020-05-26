016aa1fd238bea153b8a41b808813dee160300c4
David I. Mwangi <Dmwangi@equitybank.co.ke>
2020-05-26 19:29:02 +0300

    Add Project files

---
+++ src/main/java/com/equitybank/azurelogging/AzureloggingApplication.java
@@ -0,0 +1,13 @@
+package com.equitybank.azurelogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureloggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureloggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equitybank/azurelogging/AzureloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equitybank.azurelogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureloggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
